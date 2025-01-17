35e5000a2597582e1c8fffb8ac05cf2c6eaf7b8a
RayZh <rayzhangshanghai@hotmail.com>
2025-01-17 18:33:18 +0800

    feat: setup router

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,21 @@
 </script>
 
 <template>
+  <main>
+    <router-view />
+    <div class="footer">
+      <p>Made with ♥️ by Norb @ 2025</p>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+.footer {
+  position: fixed;
+  bottom: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+
+  font-size: 0.8rem;
+}
 </style>
