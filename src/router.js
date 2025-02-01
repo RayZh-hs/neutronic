5f4b3f21b410e539de480d7904f478cedb6a7b8a
RayZh <rayzhangshanghai@hotmail.com>
2025-02-01 23:37:16 +0800

    fix: unrecorded history and url suffix issue

--- src/router.js
+++ src/router.js
@@ -1,16 +1,18 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from './views/HomeView.vue'
 import AlbumView from './views/AlbumView.vue'
 import SubAlbumView from './views/SubAlbumView.vue'
+import CustomSelectionView from './views/CustomSelectionView.vue'
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/album', component: AlbumView },
     { path: '/album/:id', component: SubAlbumView },
+    { path: '/custom', component: CustomSelectionView },
 ]
 
 export const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
 })
