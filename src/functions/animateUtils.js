fb358e07831b81a63356e79eee4f69009bd51d2d
RayZh <rayzhangshanghai@hotmail.com>
2025-02-08 13:15:57 +0800

    feat: finish the animateUtils lib, first draft

---
+++ src/functions/animateUtils.js
@@ -0,0 +1,68 @@
+//: Constants :
+
+export const animationRefreshInterval = 1000 / 60;
+
+//: Custom easing functions :
+
+export const easeLinear = (t) => {
+    return t;
+}
+
+export const easeOutSquare = (t) => {
+    return 1 - (1 - t) * (1 - t);
+}
+
+export const easeOutCubic = (t) => {
+    return 1 - (1 - t) * (1 - t) * (1 - t);
+}
+
+export const easeOutQuart = (t) => {
+    return 1 - (1 - t) * (1 - t) * (1 - t) * (1 - t);
+}
+
+export const easeOutSine = (t) => {
+    return Math.sin(t * Math.PI / 2);
+}
+
+export const easeOutExpo = (t) => {
+    return 1 - Math.pow(2, -10 * t);
+}
+
+//: Animation tools :
+
+/**
+ * Lerp shorthand for linear interpolation.
+ * 
+ * @param {any} start The starting point;
+ * @param {any} end The ending point or destination;
+ * @param {number} ratio The blend ratio;
+ */
+const lerp = (start, end, ratio) => {
+    // return start + (end - start) * ratio;
+    return (1 - ratio) * start + ratio * end;
+}
+
+/**
+ * Animates a vue reference to a destination value with an easing function.
+ * 
+ * @param {import("vue").Ref} refObject The vue reference to animate;
+ * @param {any} dest The type refObject points to, and should be compatible with numeric multiplication;
+ * @param {number} time Time(in ms) to animate to the destination;
+ * @param {Function} easingFunction A function [0, 1] -> [0, 1 ~ ] that determines the easing of the animation;
+ */
+export const refAnimateTo =
+    (refObject, dest, time, easingFunction = easeLinear) => {
+        const start = refObject.value;
+        const animationStepsTotal = time / animationRefreshInterval;
+        let animationStep = 0;
+        
+        var animationContainer = setInterval(() => {
+            refObject.value = lerp(start, dest, easingFunction(animationStep / animationStepsTotal));
+            animationStep++;
+            if (animationStep > animationStepsTotal) {
+                // Move to end and clear interval
+                refObject.value = dest;
+                clearInterval(animationContainer);
+            }
+        }, animationRefreshInterval);
+    }
