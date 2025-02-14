582207ec88692a4615f6118a278f083b6bbb5631
RayZh <rayzhangshanghai@hotmail.com>
2025-02-14 12:28:27 +0800

    chore: handle reserved usernames

--- api/constants.cjs
+++ api/constants.cjs
@@ -1,5 +1,11 @@
 const SERVER_LISTEN_PORT = 9721;
+const reservedUsernames = [
+    "Neutronic",
+    "Admin",
+    "Administrator",
+]
 
 module.exports = {
     SERVER_LISTEN_PORT,
+    reservedUsernames,
 };
