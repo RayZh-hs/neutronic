5d3fbf68a0ef854e284fd60a40998fc1dd947993
RayZh <rayzhangshanghai@hotmail.com>
2025-02-14 12:29:27 +0800

    feat: database init on startup

--- api/server.cjs
+++ api/server.cjs
@@ -1,18 +1,24 @@
-const { SERVER_LISTEN_PORT } = require('./constants.cjs');
+const { SERVER_LISTEN_PORT, reservedUsernames } = require('./constants.cjs');
+const { initDatabases, loadPremadeLevels, getLevel } = require('./database.cjs');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
+// Init the databases
+initDatabases();
+loadPremadeLevels();    // Call this iff you are running the server the first time!
+
+// The main express app
 const app = express();
 app.use(cors());
 
-//: Favicon request
+//+ Favicon request
 app.get('/favicon.ico', (req, res) => {
     res.status(204).end();
 });
 
-//: Album request (GET)
+//+ Album request (GET)
 const albums = JSON.parse(fs.readFileSync(
     path.resolve(__dirname, "data", "albums.json")
     , "utf8")
@@ -20,9 +26,9 @@ const albums = JSON.parse(fs.readFileSync(
 app.get('/api/albums', (req, res) => {
     console.log("Albums requested");
     res.json(albums);
-}); 
+});
 
-
+//: Run server
 app.listen(SERVER_LISTEN_PORT, () => {
     console.log(`Server listening on port ${SERVER_LISTEN_PORT}`);
 });
